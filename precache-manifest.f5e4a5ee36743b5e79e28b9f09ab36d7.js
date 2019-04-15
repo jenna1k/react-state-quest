91c17de9f1e78691bafc8f456a5ecb0ac7963d94
jenna1k <jenna_kim1@hotmail.com>
2019-04-16 00:12:56 +0200

    Updates

---
+++ precache-manifest.f5e4a5ee36743b5e79e28b9f09ab36d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-state-quest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4f695b6b07b62618e1c6",
+    "url": "/react-state-quest/static/js/runtime~main.e1e2a430.js"
+  },
+  {
+    "revision": "cd0369c54fb65ccb0272",
+    "url": "/react-state-quest/static/js/main.ee430fa4.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-state-quest/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "cd0369c54fb65ccb0272",
+    "url": "/react-state-quest/static/css/main.0795a504.chunk.css"
+  },
+  {
+    "revision": "29091df0ee02ab8f2b7a5e44f66ef08a",
+    "url": "/react-state-quest/index.html"
+  }
+];
